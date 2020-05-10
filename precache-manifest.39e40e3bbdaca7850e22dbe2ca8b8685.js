d85df1d86b77934640b88c59e1af3affa4e78af9
spiridon209 <sivand888@gmail.com>
2020-05-10 23:29:17 +0500

    Updates

---
+++ precache-manifest.39e40e3bbdaca7850e22dbe2ca8b8685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f78385f139b998dc459f13c43f8f46f",
+    "url": "/Reg-Auth/index.html"
+  },
+  {
+    "revision": "11346f66007151d77c2f",
+    "url": "/Reg-Auth/static/css/2.94b6d6f9.chunk.css"
+  },
+  {
+    "revision": "16d5444e9d21b17f63ec",
+    "url": "/Reg-Auth/static/css/main.1d4aac46.chunk.css"
+  },
+  {
+    "revision": "11346f66007151d77c2f",
+    "url": "/Reg-Auth/static/js/2.554b6705.chunk.js"
+  },
+  {
+    "revision": "bf72f5949af4b9e31beb2ebb9b65018f",
+    "url": "/Reg-Auth/static/js/2.554b6705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d5444e9d21b17f63ec",
+    "url": "/Reg-Auth/static/js/main.53b5c53d.chunk.js"
+  },
+  {
+    "revision": "50988dc6b0ea942448e0",
+    "url": "/Reg-Auth/static/js/runtime-main.9c921979.js"
+  }
+]);
