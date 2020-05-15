b615abc91f62576ca2e19410b969e967e3926f7c
spiridon209 <sivand888@gmail.com>
2020-05-15 21:13:56 +0500

    -- Added autologin feature

--- src/api/authRequest.js
+++ src/api/authRequest.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 import rootApiUrl from './rootApiUrl';
 
-const authUrl = `${rootApiUrl}/users/login`;
-
 export const authFetch = async (data) => {
+  const authUrl = `${rootApiUrl}/users/login`;
   const response = await axios.post(authUrl, {
     user: data,
   });
   return response;
 };
 
-const regUrl = `${rootApiUrl}/users/`;
-
 export const regFetch = async (data) => {
+  const regUrl = `${rootApiUrl}/users`;
   const response = await axios.post(regUrl, {
     user: data,
   });
+  return response;
+};
+
+export const autoLogInFetch = async (headers) => {
+  const autoLogInUrl = `${rootApiUrl}/user`;
+  const response = await axios.get(autoLogInUrl, { headers });
   return response;
 };
